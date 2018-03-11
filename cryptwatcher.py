fd83be1d10e08c532402d9320f8a18065b410673
gamma-github <abe89183@gmail.com>
2018-03-11 18:54:21 +0900

    Fix cryptwatcher
    I was going to have added coincheck_cryptwatcher. So I added that again.

--- cryptwatcher.py
+++ cryptwatcher.py
@@ -6,15 +6,21 @@ import pandas as pd
 import pybitflyer
 #Zaifにアクセスするのに使う
 from zaifapi.impl import ZaifPublicApi
+#coincheckにアクセスするのに使う
+from coincheck.coincheck import CoinCheck
+import logging
 # 時間の管理に使う
 import time
 from datetime import datetime
 #データのプロットに使う
 import matplotlib
 import matplotlib.pyplot as plt
+#型変換に使う
+import ast
 
 bitflyerapi = pybitflyer.API()
 zaifapi = ZaifPublicApi()
+coinCheck = CoinCheck('ACCES_KEY','API_SECRET')
 
 bitflyer_raws = []
 zaif_raws = []
@@ -36,10 +42,14 @@ while True:
         Quoinex_Markets_BTCJPY = requests.get('https://api.quoine.com/products/code/CASH//BTCJPY')
         quoinex_tick = json.loads(Quoinex_Markets_BTCJPY.text)
         print("{}".format(json.dumps(quoinex_tick, indent=4)))
+        coincheck_tick = coinCheck.ticker.all()
+        print(coincheck_tick)
+        coincheck_tick_dic = ast.literal_eval(coincheck_tick)
         #データの更新
         bitflyer_raws = np.append(bitflyer_raws, bitflyer_tick['ltp'])
         zaif_raws = np.append(zaif_raws, zaif_last_price['last_price'])
         quoinex_raws = np.append(quoinex_raws, quoinex_tick['last_traded_price'])
+        coincheck_raws = np.append(coincheck_raws, coincheck_tick_dic['last'])
         timestamp = np.append(timestamp, datetime.now())
         # 次の00秒まで休憩
         time.sleep(57)
@@ -47,7 +57,7 @@ while True:
         elapsed_time = time.time() - start
         if elapsed_time > 60*t:#t分後にデータを処理してbreak
             #pandasを使って処理
-            df = pd.DataFrame({'bitflyer':bitflyer_raws,'zaif':zaif_raws,'quoinex':quoinex_raws}, index = timestamp)
+            df = pd.DataFrame({'bitflyer':bitflyer_raws,'zaif':zaif_raws,'quoinex':quoinex_raws,'coincheck':coincheck_raws}, index = timestamp)
             print(df)
             #csv形式で保存
             df.to_csv("result.csv")
